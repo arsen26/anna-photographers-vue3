4fe9f94d6fe60b9b908c2b62f2f33f3958bd7d91
unknown <arsencenollari89910@gmail.com>
2025-01-18 22:34:17 +0100

    menu almost finished

--- src/components/siteHeader.vue
+++ src/components/siteHeader.vue
@@ -1,132 +1,104 @@
 <template>
-  <div>
-    <v-navigation-drawer v-model="drawer" location="left" :temporary="true">
-      <v-list lines="one">
-        <template v-for="(item, i) in items">
-          <v-list-item v-if="!item.submenu" :to="item.to" title="">
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ item.title.toUpperCase() }}</v-list-item-title>
-          </v-list-item>
-
-          <v-list-group v-else :prepend-icon="item.icon" title="" no-action>
-            <template #activator>
-              <v-list-item-title>
-                {{ item.title.toUpperCase() }}
-              </v-list-item-title>
-            </template>
-
-            <v-list-item v-for="child in item.submenu" :key="child.title" :to="child.to">
-              <v-list-item-title>{{ child.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list-group>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-
-    <!-- MOBILE VIEW -->
-    <v-app-bar :absolute="true" scroll-behavior="hide" height="74" elevate-on-scroll>
-      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = true" />
+  <!-- Desktop VIEW -->
+  <v-app-bar scroll-behavior="hide" height="74" elevate-on-scroll>
+    <v-row class="row-container">
       <v-img class="logo-style-menu" :src="image" />
-      <v-spacer />
-      <template v-for="(name, menuitem) in items">
-        <template v-if="name.submenu">
-          <v-menu :key="menuitem" open-on-hover offset-y transition="slide-y-transition" bottom>
-            <template #activator="{ on, attrs }">
-              <v-btn
-                plain
-                class="py-5 submenubtn hidden-sm-and-down"
-                :ripple="false"
-                v-bind="attrs"
-                style="height: auto"
-                v-on="on"
-              >
-                {{ name.title }}
-                <v-icon right small class="mx-0"> mdi-chevron-down </v-icon>
-              </v-btn>
-            </template>
-            <v-list dense>
-              <v-list-item v-for="(item, index) in name.submenu" :key="index" link :to="item.to">
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </template>
-        <v-btn @click="navigateTo(name.to)" v-else :key="menuitem" >{{ name.title }}</v-btn>
-        </template
-      ><v-spacer />
+      <v-spacer class="if-small-screen" />
+      <v-app-bar-nav-icon
+        class="menu-mobile-style"
+        variant="text"
+        @click.stop="changeDrawerValue"
+      ></v-app-bar-nav-icon>
 
-      <!-- <v-btn icon @click="changeThemeColor">
-        <v-icon>{{
-          $vuetify.theme.dark ? 'mdi-white-balance-sunny' : 'mdi-weather-night'
-        }}</v-icon>
-      </v-btn> -->
-    </v-app-bar>
-  </div>
+      <template v-for="(item, i) in items" :key="i">
+        <v-btn class="submenubtn" @click="navigateTo(item.to)">
+          {{ item.title }}
+        </v-btn>
+      </template>
+    </v-row>
+  </v-app-bar>
+
+  <!-- Mobile Drawer -->
+  <v-navigation-drawer v-model="drawer" location="right" temporary>
+    <v-list lines="one">
+      <template v-for="(item, i) in items" :key="i">
+        <v-list-item :prepend-icon="item.icon" :to="item.to">
+          <v-list-item-title class="text-uppercase menu-text-mobile">{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import annaLogo from '@/assets/annalogo.png'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
 
 const router = useRouter()
 const image = ref(annaLogo)
 const drawer = ref(false)
 const items = ref([
-  {
-    icon: 'mdi-folder-home-outline',
-    title: 'Home',
-    to: '/',
-  },
-  {
-    icon: 'mdi-account',
-    title: 'Rreth nesh',
-    to: '/About',
-  },
-  {
-    icon: 'mdi-folder-image',
-    title: 'Photography Gallery',
-    to: '/gallery',
-  },
-  {
-    icon: 'mdi-cash-usd',
-    title: 'Anna dekor',
-    to: '/pricing',
-  },
-  {
-    icon: 'mdi-folder-image',
-    title: 'Decor Gallery',
-    to: '/gallery',
-  },
-  {
-    icon: 'mdi-contacts',
-    title: 'Contact',
-    to: '/Contact',
-  },
+  { icon: 'mdi-home-outline', title: 'Home', to: '/' },
+  { icon: 'mdi-information-outline', title: 'Rreth nesh', to: '/About' },
+  { icon: 'mdi-view-gallery-outline', title: 'Photography Gallery', to: '/gallery' },
+  { icon: 'mdi-finance', title: 'Anna dekor', to: '/pricing' },
+  { icon: ' mdi-view-gallery', title: 'Decor Gallery', to: '/gallery' },
+  { icon: 'mdi-contacts', title: 'Contact', to: '/Contact' },
 ])
-
-// const changeThemeColor = () => {
-//   if (this.$vuetify.theme.dark) {
-//     this.$vuetify.theme.dark = false;
-//   } else {
-//     this.$vuetify.theme.dark = true;
-//   }
-// };
-console.log(router, '=-=> router')
+const changeDrawerValue = () => {
+  console.log(drawer.value)
+  drawer.value = !drawer.value
+}
 const navigateTo = (path) => {
-  router.push({path})
-};
-
-
+  router.push({ path })
+}
 </script>
 
 <style scoped>
+/* Mobile Styles */
+@media (max-width: 960px) {
+  .menu-text-mobile{
+    margin-left: 0px !important;
+  }
+  .menu-mobile-style {
+    visibility: visible;
+    display: block;
+  }
+
+  .logo-style-menu {
+    margin-right: 40px !important;
+    max-width: 70px;
+    max-height: 50px;
+  }
+  .row-container {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .submenubtn {
+    display: none;
+  }
+}
+
+/* Desktop Styles */
+@media (min-width: 961px) {
+  .menu-mobile-style {
+    visibility: hidden;
+    display: none;
+  }
+  .if-small-screen {
+    display: none;
+  }
+
+  .submenubtn {
+    display: inline-flex;
+  }
+}
+
 .logo-style-menu {
   height: auto !important;
   width: auto;
-  max-height: 70px;
+  max-height: 100px;
   max-width: 100px;
   object-fit: contain !important;
   margin-left: 100px;
@@ -134,5 +106,11 @@ const navigateTo = (path) => {
 
 .submenubtn {
   cursor: default;
+}
+.row-container {
+  justify-content: center;
+  align-items: center;
+  flex: auto;
+  margin-left: -130px;
 }
 </style>
